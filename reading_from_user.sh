a6b35a937215fd1235c733d56ca18c76f8c729a2
prashanth <prashanthe31@gmail.com>
2023-06-02 13:58:57 +0530

    first commit: 2 files

---
+++ reading_from_user.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo -e "\n"
+which bash
+
+echo -n "Please enter your first name: "
+read First_Name
+
+echo -n "Please enter your second name: "
+read Second_Name
+
+echo -e "Hello $First_Name $Second_Name, This is bash variables\n" 
+
+
