a89e6c0e68340383d65c47c7eb1357e8e1cd7801
prashanth-habib <prashanthe31@gmail.com>
2023-06-07 10:22:50 +0000

    Added files related to class 3

---
+++ class_3/Readme.txt
@@ -0,0 +1,2 @@
+This folder contains the scripts related to class 3.
+Link to class on YouTube: https://youtu.be/uQE_4Q-HZZw
---
+++ class_3/myscript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+now=$(date)
+my_name="Prashanth"
+my_age=23
+
+echo -e "\nMy name is $my_name and I am $my_age years old"
+echo -e "system time is\n $now \n"
+echo -e "Your username $USER and you are using $SHELL\n"
