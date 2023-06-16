6b3a2e4375598ce6895ef5024ab69d8e731af97b
prashanth-habib <prashanthe31@gmail.com>
2023-06-16 10:08:41 +0000

    added bash script for intalling prerequisites.

---
+++ MakeDemo/makeprep.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Read input from the user
+read -p "Enter your answer (yes/no): " answer
+
+# Check if the input is "yes" or "no"
+if [[ $answer == "y" ]]; then
+    sudo apt install make && sudo apt instalp g++
+elif [[ $answer == "n" ]]; then
+    echo "You answered no."
+else
+    echo "Invalid input. Please enter either 'y' or 'n'."
+fi
+
