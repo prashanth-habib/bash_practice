bee81d6a67ed94728d957dbbb89d74901ca99eda
prashanth-habib <prashanthe31@gmail.com>
2023-06-19 08:48:38 +0000

    added cmake directory

---
+++ cmake_demo/Readme.md
@@ -0,0 +1,5 @@
+This directory contains the files needed for CMake demo.
+Run the commands in below order:
+1. cmake .
+2. make
+3. ./hello
---
+++ cmake_demo/message.h
@@ -0,0 +1,7 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+class message{
+     public:
+           void printMessage();
+};
+#endif // MESSAGE_H
